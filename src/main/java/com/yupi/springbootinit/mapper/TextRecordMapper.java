8ade32b45f340d68c22adf427d5fa40be827360c
LaHero <3421449006@qq.com>
2024-02-21 22:20:56 +0800

    删减了部分不必要代码，解决了 rabbitmq 存在的部分问题。成功上线了项目

---
+++ src/main/java/com/yupi/springbootinit/mapper/TextRecordMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.TextRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 86182
+* @description 针对表【text_record(文本记录表)】的数据库操作Mapper
+* @createDate 2024-02-17 23:09:08
+* @Entity com.yupi.springbootinit.model.entity.TextRecord
+*/
+public interface TextRecordMapper extends BaseMapper<TextRecord> {
+
+}
+
+
+
+
